35edd6d24a50aa7068265e8d31cabb6ed9cfb271
Michael Valancius <42551234+mvalancius@users.noreply.github.com>
2019-04-30 03:40:11 -0400

    Update vbae.py

--- fvbae/vbae.py
+++ fvbae/vbae.py
@@ -4,7 +4,51 @@ import numpy as np
 
 def vae(Xtrain, nndim = 200, latentdim = int(2), samplestd = 1.0,
         epochs = int(10), batch_size = int(100), optimizer = "Adamax", return_type = "all"):
-    """Implementation of an auto-encoder following AEVB"""
+    """
+    vae(Xtrain, nndim = 200, latentdim = int(2), samplestd = 1.0, epochs = int(10), batch_size = int(100), optimizer = "Adamax", return_type = "all")
+    
+    Add arguments element wise.
+    
+    Parameters
+    -----------
+    Xtrain : 2D numpy array
+        Rows are observations and columns are features. Only float values.
+    nndim : integer
+        number of hidden units in neural network. 
+    latentdim : integer
+        Number of latent dimensions. Must be less than Xtrain feature size and nndim.
+    samplestd : float
+        standard deviation of normal distribution chosen for latent dimension, where mean is zero.
+    epochs : integer
+        Passes through dataset, as defined by Keras. Must be greater than 0
+    batch_size : integer
+        Mini-batch size. Must be greater than zero and less than or equal to number of observations.
+    optimizer : string of Keras optimizer or Keras optimizer object
+        Optimizer passed to Keras. If string, defaults for that optimizer are chosen.
+        If using Keras optimizer object, more control (such as learning rate) is allowed.
+        Follows typical Keras notation. Further details available here: https://keras.io/optimizers/
+    return_type : string
+        Either "autoencoder", "encoder", "decoder" or other string / None.
+        Allows for the return of only one of the Keras objects. If none of
+        "autoencoder", "encoder", or "decoder" is specified, all are returned.
+        
+    Returns
+    -----------
+    Trained Keras model. Return type specified by return_type parameter. 
+        Model can then be used as if it were a manually trained Keras model and all usual methods apply
+        
+    Notes
+    --------
+    When using predict method on returned model, ensure batch size is the same as specified in vae function
+    
+    Example:
+    
+    >>> X = np.random.normal(loc = 1, scale = 2, size = (100000, 500))
+    >>> opt = keras.optimizers.Adamax(lr=0.001, beta_1=0.9, beta_2=0.999, decay=0.0)
+    autoencoder, encoder, decoder = fvbae.vae(X, nndim = 100, latentdim = 5, optimizer = opt,
+        epochs = 100, batch_size = int(100))
+        
+    """
     
     inputdim = Xtrain.shape[1]
 
